4afae2b3ac9862c3571dd24233bc8ba4f28a945d
Aman Goyal <amangoyal1412@gmail.com>
2017-05-21 16:47:37 +0530

    Initial commit

---
+++ nobel-prep.py
@@ -0,0 +1,13 @@
+# http://api.nobelprize.org/v1/prize.json?year=1901&yearTo=1910&category=physics
+# http://api.nobelprize.org/v1/country.json
+
+class NobelPrize(object):
+    def __init__(self):
+        pass
+
+    def prize(self, start_year, category="Physics", end_year=None):
+        pass
+    def laureate():
+        pass
+    def country():
+        pass
